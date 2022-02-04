3101e9eec107e06ea55303a779e29cbfec35ad2b
beataj <beata@emc2wave.com>
2022-02-03 23:04:48 -0800

    more complex types

--- basics.ts
+++ basics.ts
@@ -15,3 +15,24 @@ userName = 'beata';
 let isInstructor: boolean;
 
 isInstructor = true;
+
+// More complex
+
+let hobbies: string[];
+
+hobbies = ['Sports', 'Cooking'];
+
+let person: {
+    name: string,
+    age: number
+};
+
+person = {
+    name: 'Max',
+    age: 32 
+}
+
+let people: {
+    name: string,
+    age: number
+}[];
