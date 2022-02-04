07ddeb40b36665ad3f19afdf156582c2618883df
beataj <beata@emc2wave.com>
2022-02-03 22:56:36 -0800

    numbers, string, boolean

--- basics.ts
+++ basics.ts
@@ -1,3 +1,17 @@
 // Primitives: number, string, boolean
 // More complex types: arrays, objects
 // Function types, parameters
+
+// Primitves
+
+let age: number;
+
+age = 12
+
+let userName: string;
+
+userName = 'beata';
+
+let isInstructor: boolean;
+
+isInstructor = true;
